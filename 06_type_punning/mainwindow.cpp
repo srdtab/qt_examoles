c26e0f45919fc3947c4ceb10f5476b3b5ca8b045
Dmitry Sorokin <srdtab@gmail.com>
2021-02-24 17:52:19 +0300

    Долго ковырялся и пытался понять, почему type punning не работает. Через дебаггер выяснил, что дело в том, что у нас little-endian

--- 06_type_punning/mainwindow.cpp
+++ 06_type_punning/mainwindow.cpp
@@ -13,3 +13,64 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+S_PDCM MainWindow::punPDCM(const QVector<int> &SDs)
+{
+    S_PDCM pdcm;
+    int length = SDs.length();
+    quint16 mkoWords[length];
+    for(int i = 0; i < length; i++){
+        mkoWords[i] = static_cast<quint16>(SDs[i]);
+    }
+
+    for(int i = 0; i < length; i++){
+        qDebug()<<"~"<<i<<mkoWords[i];
+    }
+
+    //Пока работаю дома и не имею доступа к ПИЛВ
+    //считаю, что:
+    //сд 0-3   - Х  (double)
+    //сд 4-7   - Y  (double)
+    //сд 8-11  - Z  (double)
+    //сд 12-13 - VХ (float)
+    //сд 14-15 - VY (float)
+    //сд 16-17 - VZ (float)
+
+    pdcm.Y = *(reinterpret_cast<double* >(&mkoWords[0]));  //Координаты - double'ы по IEEE 754 (64 бита, 4 слова МКО)
+    pdcm.Y = *(reinterpret_cast<double* >(&mkoWords[4]));
+    pdcm.Z = *(reinterpret_cast<double* >(&mkoWords[8]));
+    pdcm.VX = *(reinterpret_cast<float* >(&mkoWords[12])); //Скорости - float'ы по IEEE 745 (32 бита, 2 слова МКО)
+    pdcm.VY = *(reinterpret_cast<float* >(&mkoWords[14]));
+    pdcm.VZ = *(reinterpret_cast<float* >(&mkoWords[16]));
+
+    qDebug()<<pdcm.X<<pdcm.Y<<pdcm.Z<<pdcm.VX<<pdcm.VY<<pdcm.VZ;
+
+    return pdcm;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+
+    SD.clear();
+    for(int i = 0; i< 30; i++){
+        SD << qrand()%0x10000;
+    }
+    qDebug()<<"Сгенерировали массив:";
+    qDebug()<<SD;
+    for(int i = 0; i< 30; i++){
+        qDebug()<<i<<QString::number((int)SD[i],2).rightJustified(16,'0');
+    }
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    auto pdcm = punPDCM(SD);
+    qDebug()<<"После преобразования получили:";
+    qDebug()<<pdcm.X<<pdcm.Y<<pdcm.Z<<pdcm.VX<<pdcm.VY<<pdcm.VZ;
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    uint16_t x = 0x0001;
+    qDebug()<<QString("%1-endian").arg(*((uint8_t *) &x) ? "little" : "big");
+}
--- 06_type_punning/mainwindow.h
+++ 06_type_punning/mainwindow.h
@@ -2,10 +2,21 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QVector>
+#include <QDebug>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
+
+struct S_PDCM{
+    double X{0};
+    double Y{0};
+    double Z{0};
+    float VX{0};
+    float VY{0};
+    float VZ{0};
+};
 
 class MainWindow : public QMainWindow
 {
@@ -15,7 +26,19 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    static S_PDCM punPDCM(const QVector<int>& SDs);
+    static double punDouble(const int* beginning);
+    static float punFloat(const int* beginning);
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QVector<int> SD; //В таком же формате, в котором возвращает библиотека МКО
 };
 #endif // MAINWINDOW_H
